4d77d342ddbcbe43e9ea7453d38486c5c7e01794
Domenic Simone <domenicsim1@gmail.com>
2019-10-11 23:45:47 +1100

    line position support, lexer

--- src/monkey/lexer/lexer.go
+++ src/monkey/lexer/lexer.go
@@ -9,13 +9,14 @@ import (
 type Lexer struct {
 	input        []rune
 	position     int
-	readPosition int // TODO: Track line number
+	readPosition int
+	linePosition int
 	ru           rune
 }
 
 // New *Lexer
 func New(input string) *Lexer {
-	l := &Lexer{input: []rune(input)}
+	l := &Lexer{input: []rune(input), linePosition: 1}
 	l.readRune()
 	return l
 }
@@ -32,66 +33,70 @@ func (l *Lexer) NextToken() token.Token {
 			ch := l.ru
 			l.readRune()
 			literal := string(ch) + string(l.ru)
-			tok = token.Token{Type: token.EQ, Literal: literal}
+			tok = token.Token{Type: token.EQ, Literal: literal, Line: l.linePosition}
 		} else {
-			tok = newToken(token.ASSIGN, l.ru)
+			tok = newToken(token.ASSIGN, l.ru, l.linePosition)
 		}
 	case ';':
-		tok = newToken(token.SEMICOLON, l.ru)
+		tok = newToken(token.SEMICOLON, l.ru, l.linePosition)
 	case '(':
-		tok = newToken(token.LPAREN, l.ru)
+		tok = newToken(token.LPAREN, l.ru, l.linePosition)
 	case ')':
-		tok = newToken(token.RPAREN, l.ru)
+		tok = newToken(token.RPAREN, l.ru, l.linePosition)
 	case ',':
-		tok = newToken(token.COMMA, l.ru)
+		tok = newToken(token.COMMA, l.ru, l.linePosition)
 	case ':':
-		tok = newToken(token.COLON, l.ru)
+		tok = newToken(token.COLON, l.ru, l.linePosition)
 	case '+':
-		tok = newToken(token.PLUS, l.ru)
+		tok = newToken(token.PLUS, l.ru, l.linePosition)
 	case '-':
-		tok = newToken(token.MINUS, l.ru)
+		tok = newToken(token.MINUS, l.ru, l.linePosition)
 	case '{':
-		tok = newToken(token.LBRACE, l.ru)
+		tok = newToken(token.LBRACE, l.ru, l.linePosition)
 	case '}':
-		tok = newToken(token.RBRACE, l.ru)
+		tok = newToken(token.RBRACE, l.ru, l.linePosition)
 	case '[':
-		tok = newToken(token.LBRACKET, l.ru)
+		tok = newToken(token.LBRACKET, l.ru, l.linePosition)
 	case ']':
-		tok = newToken(token.RBRACKET, l.ru)
+		tok = newToken(token.RBRACKET, l.ru, l.linePosition)
 	case '!':
 		if l.peekRune() == '=' {
 			ch := l.ru
 			l.readRune()
 			literal := string(ch) + string(l.ru)
-			tok = token.Token{Type: token.NOTEQ, Literal: literal}
+			tok = token.Token{Type: token.NOTEQ, Literal: literal, Line: l.linePosition}
 		} else {
-			tok = newToken(token.BANG, l.ru)
+			tok = newToken(token.BANG, l.ru, l.linePosition)
 		}
 	case '*':
-		tok = newToken(token.ASTERISK, l.ru)
+		tok = newToken(token.ASTERISK, l.ru, l.linePosition)
 	case '/':
-		tok = newToken(token.SLASH, l.ru)
+		tok = newToken(token.SLASH, l.ru, l.linePosition)
 	case '<':
-		tok = newToken(token.LT, l.ru)
+		tok = newToken(token.LT, l.ru, l.linePosition)
 	case '>':
-		tok = newToken(token.GT, l.ru)
+		tok = newToken(token.GT, l.ru, l.linePosition)
 	case '"':
 		tok.Type = token.STRING
 		tok.Literal = l.readString()
+		tok.Line = l.linePosition
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+		tok.Line = l.linePosition
 	default:
 		if isLetter(l.ru) {
 			tok.Literal = l.readIdentifier()
 			tok.Type = token.LookupIdent(tok.Literal)
+			tok.Line = l.linePosition
 			return tok
 		} else if isDigit(l.ru) {
 			tok.Type = token.INT
 			tok.Literal = l.readNumber()
+			tok.Line = l.linePosition
 			return tok
 		}
-		tok = newToken(token.ILLEGAL, l.ru)
+		tok = newToken(token.ILLEGAL, l.ru, l.linePosition)
 	}
 
 	l.readRune()
@@ -118,6 +123,15 @@ func (l *Lexer) readRune() {
 
 func (l *Lexer) skipWhitespace() {
 	for l.ru == ' ' || l.ru == '\t' || l.ru == '\n' || l.ru == '\r' {
+		switch l.ru {
+		case '\r':
+			if l.peekRune() == '\n' {
+				l.readRune()
+			}
+			l.linePosition++
+		case '\n':
+			l.linePosition++
+		}
 		l.readRune()
 	}
 }
@@ -153,8 +167,8 @@ func isLetter(ru rune) bool {
 	return ru == '_' || unicode.IsLetter(ru)
 }
 
-func newToken(tokenType token.Type, ru rune) token.Token {
-	return token.Token{Type: tokenType, Literal: string(ru)}
+func newToken(tokenType token.Type, ru rune, line int) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ru), Line: line}
 }
 
 func isDigit(ru rune) bool {
