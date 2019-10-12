e203e7fe0a3dfb0cac42a7fa8f927b2dcd73d74b
Domenic Simone <domenicsim1@gmail.com>
2019-10-13 02:15:43 +1100

    Test fix; Quote

--- src/monkey/evaluator/evaluator.go
+++ src/monkey/evaluator/evaluator.go
@@ -83,6 +83,9 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		return &object.Function{Parameters: params, Env: env, Body: body}
 
 	case *ast.CallExpression:
+		if node.Function.TokenLiteral() == "quote" {
+			return quote(node.Arguments[0])
+		}
 		function := Eval(node.Function, env)
 		if isError(function) {
 			return function
@@ -118,6 +121,7 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 
 	case *ast.HashLiteral:
 		return evalHashLiteral(node, env)
+
 	}
 
 	return nil
